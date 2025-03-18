97ede3914175742e3e2e83c8205bfe6b386e310b
Gaurav Singh Solanki <gauravpanahera@gmail.com>
2025-03-18 20:43:42 +0530

    first change

--- src/component/Profile/ProfileComp/ProfileHeader.jsx
+++ src/component/Profile/ProfileComp/ProfileHeader.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import styles from "./Profile.module.css";
 import { useAuth } from "../../Layout/AuthContext";
-
-import { SlPrinter } from "react-icons/sl";
+import { SlPrinter } from "react-icons/sl"; // Import print icon
 
 const ProfileHeader = () => {
   const { profile, fetchprofile } = useAuth();
@@ -12,11 +11,19 @@ const ProfileHeader = () => {
   }, []);
 
   if (!profile) {
-    return <></>;
+    return null;
   }
 
   return (
-    <div className={`${styles.profileHeader}`}>
+    <div className={`${styles.profileHeader} relative`}>
+      {/* Print Button with Icon */}
+      <button
+        onClick={() => window.print()}
+        className="absolute top-2 right-2 bg-white p-1 rounded-full shadow-md hover:bg-gray-200 transition"
+      >
+        <SlPrinter className="text-gray-600 text-xl" /> print
+      </button>
+
       <div className={styles.relative}>
         <img
           src={profile}
